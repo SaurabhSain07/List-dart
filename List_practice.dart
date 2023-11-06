712b07bedea1caa5138368f7c5f6d8ebf2d7097e
Saurabh Sain <visual.sain100486@gmail.com>
2023-11-06 16:42:11 +0530

    add list become

---
+++ List_practice.dart
@@ -0,0 +1,10 @@
+void main() {
+  List Studentname = ["Saurabh Sain", "Vishal Sain", "Gopal Saini", 12];
+  // List<String> Studentname = ["Saurabh Sain", "Vishal Sain", "Gopal Saini", 12];
+  // print(Studentname.length);
+  // print(Studentname.first);
+  // print(Studentname.last);
+  // Studentname[0] = "Ram Kumar";
+  // print(Studentname[Studentname.length - 2]);
+  print(Studentname);
+}
