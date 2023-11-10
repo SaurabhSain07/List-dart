45c659702b44a75e857ffb764ea92cec670d8420
Saurabh Sain <visual.sain100486@gmail.com>
2023-11-10 11:58:01 +0530

    ram

--- pattern01.dart
+++ pattern01.dart
@@ -1,6 +1,20 @@
 import 'dart:io';
 
 void main() {
+  invertedpyramid();
+  print("----------");
+  rotatedSimplePyramid();
+}
+
+// inverted pyramid
+
+// *****
+// ****
+// ***
+// **
+// *
+
+invertedpyramid() {
   for (var i = 6; i >= 1; i--) {
     for (var j = i; j >= 1; j--) {
       stdout.write("*");
@@ -9,8 +23,19 @@ void main() {
   }
 }
 
-// *****
-// ****
-// ***
-// **
-// *
+// rotatedSimplePyramid *
+//                    * *
+//                  * * *
+//                * * * *
+
+rotatedSimplePyramid() {
+  var rows = 5;
+  var ss = (" ");
+  for (var a = 1; a < rows; a++) {
+    stdout.write("${ss * (rows - 1)}");
+    for (var b = 1; b <= a; b++) {
+      stdout.write("*");
+    }
+    print("");
+  }
+}
